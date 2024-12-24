1b32440356d87814a2b8b82f1a74b438e74c2db3
Ivan Panchev <190841820+ivanpanchevv@users.noreply.github.com>
2024-12-24 16:36:30 +0100

    Update Main.java

--- main/java/Main.java
+++ main/java/Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // Create a shared Scanner instance
-        Menu menu = new Menu(scanner); // Pass the Scanner to the Menu
-        menu.displayMenu(); // Display the menu once
-        scanner.close(); // Close the Scanner after the program finishes
+        new DatabaseConnection();
+        Scanner scanner = new Scanner(System.in);
+        Menu menu = new Menu(scanner);
+        menu.displayMenu();
+        scanner.close();
     }
 }
+
