5c89235daebf35c3a90c0d59573aa3ee96be40e5
Ivan Panchev <190841820+ivanpanchevv@users.noreply.github.com>
2024-12-24 20:57:37 +0200

    commented the bd connection becaus everyone should first create a connection locally

--- main/java/Main.java
+++ main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        new DatabaseConnection();
+       // new DatabaseConnection();
         Scanner scanner = new Scanner(System.in);
         Menu menu = new Menu(scanner);
         menu.displayMenu();
