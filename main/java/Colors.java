1e5489ce46239123ba1f1e63a2e7377717e58c29
ivanpanchevv <ipanchev13@gmail.com>
2024-12-24 16:33:42 +0100

    Update and rename ANSI.java to Colors.java

--- main/java/Colors.java
+++ main/java/Colors.java
@@ -1,4 +1,4 @@
-public class ANSI {
+public class Colors {
     public static final String RESET ="\u001B[0m";
     public static final String RED ="\u001B[31m";
     public static final String BLUE ="\u001B[34m" ;
